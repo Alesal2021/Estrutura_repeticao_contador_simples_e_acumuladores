880bdccdcace876f982773d021ae608c7ac26b89
Alesal2021 <alessandrocontato1975@gmail.com>
2022-02-03 15:31:52 -0300

    Initial Commit

---
+++ Valores TRUTHY e FALSEY.py
@@ -0,0 +1,13 @@
+#O VALOR DA CONDICIONAL AQUI E FALSE
+nome = ''
+# NOME VAI FICAR WHILE TRUE NA VERDADE
+while not nome:
+    nome = input('digite seu nome:')
+valor = int(input('Digite um valor:'))
+# ESSE IF VAI TRATAR O VALOR COM TRUE
+if valor:
+    print('Voce digitou um numero diferente de 0')
+else:
+    print('Voce digitou 0 ')
+
+
