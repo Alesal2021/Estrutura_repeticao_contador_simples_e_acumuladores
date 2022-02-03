8e4e04d92ef7ce751416b3b41481f0eabf91c923
Alesal2021 <alessandrocontato1975@gmail.com>
2022-02-03 16:58:05 -0300

    Initial Commit

---
+++ TABUADA COM O FOR.py
@@ -0,0 +1,5 @@
+for tabuada in range(1,11,1):
+    print('Qual a Tabuada= {}'.format(tabuada))
+    for i in range(1,11,1):
+        print('{} X {} = {}'.format(tabuada,i,tabuada * i))
+    tabuada += 1
