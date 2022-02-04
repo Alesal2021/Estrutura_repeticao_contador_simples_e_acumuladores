f752bcdb6ef37f0247c6f86fbb034de3965e321e
Alesal2021 <alessandrocontato1975@gmail.com>
2022-02-04 11:39:41 -0300

    Initial Commit

---
+++ Sequencia de prints.py
@@ -0,0 +1,19 @@
+
+s1 = 3
+while s1 <= 12:
+    print(s1)
+    s1 += 1
+
+for i in range(3,13,1):
+    print(i)
+
+
+s1 = 0
+while s1 < 9:
+    print(s1)
+    s1 += 2
+
+for i in range(0,9,2):
+    print(i)
+
+
